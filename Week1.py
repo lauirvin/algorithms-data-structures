19a8ffe1b92b77bc163c19180272fb7bcadaaee1
lauirvin <lauirvin98@gmail.com>
2017-10-11 23:21:12 +0100

    Completed Week1.py question 1

--- Week1.py
+++ Week1.py
@@ -1,27 +1,28 @@
 # Week 1 Lab
 
-# Write a method that combines two strings, by taking one character from the first string, then one from the second string and so on. Once one string has no characters left it should carry on with the other string.
+# 1. Write a method that combines two strings, by taking one character from the first string, then one from the second string and so on. Once one string has no characters left it should carry on with the other string.
 
-def merge(l, m):
-    result = []
-    i = j = 0
-    total = len(l) + len(m)
-    while len(result) != total:
-        if len(l) == i:
-            result += m[j:]
-            break
-        elif len(m) == j:
-            result += l[i:]
-            break
-        elif l[i] < m[j]:
-            result.append(l[i])
-            i += 1
-        else:
-            result.append(m[j])
-            j += 1
-    return result
+def mergeStrings():
+    string1 = input("Please enter String 1: ")
+    string2 = input("Please enter string 2: ")
+    string1List = []
+    string2List = []
 
+    if len(string1) == 0:
+        pass
+    else:
+        for char in string1:
+            string1List.append(char)
 
-extendedString()
+    if len(string2) == 0:
+        pass
+    else:
+        for char in string2:
+            string2List.append(char)
 
+    mergedStringList = [j for i in zip(string1List, string2List) for j in i]
 
+    mergedString = ''.join(mergedStringList)
+    print("Your combined string is: ", mergedString)
+
+mergeStrings()
