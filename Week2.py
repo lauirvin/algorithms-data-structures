4e86856c9bcf187c525e58c712f56eb7c871dbbb
lauirvin <lauirvin98@gmail.com>
2017-10-19 10:30:29 +0100

    Week 2 lab sheet

--- Week2.py
+++ Week2.py
@@ -3,9 +3,11 @@
 # 1. Read the degree of two polynomials and their coefficients, all integers, from the standard input.
 
 # a) Write the pseudocode for adding two polynomials - Written out in Python for algorithm testing.
-def polynomials(p1, p2, degree1, degree2):
+
+
+def polynomialAddition(p1, p2, degree1, degree2):
     output = []
-    
+
     if degree1 < degree2:
         output.insert(0, p2[0])
         degreeDifference = degree2 - degree1
@@ -19,4 +21,24 @@ def polynomials(p1, p2, degree1, degree2):
     print("The Res is: ", output)
     print("The Degree is: ", finalDegree)
 
-polynomials([3,1,9], [2,1,2,1], 2, 3)
+#polynomialAddition([3,1,9], [2,1,2,1], 2, 3)
+
+# b) Given the following pseudocode, first understand how it works, then implement the code for multiplying the two polynomials in the programming language of your choice
+
+
+def polynomialMultiplication(p1, p2, degree1, degree2):
+
+    res = [0] * (degree1 + degree2 + 1)
+    print(res)
+
+    for i in p1:
+        res = i + j
+        res = res[i + j] + (p1**i * p2**j)
+        for j in p2:
+            res = i + j
+            res = res[i + j] + (p1**i * p2**j)
+
+    print(res)
+
+
+polynomialMultiplication([3, 1, 9], [2, 1, 2, 1], 2, 3)
