6ff385a252f0ead27b62fc4331aa11f9b00769b6
lauirvin <lauirvin98@gmail.com>
2017-10-21 16:32:23 +0100

    Final commit for Week2.py and Week2.md

--- Week2.py
+++ Week2.py
@@ -21,7 +21,7 @@ def polynomialAddition(p1, p2, degree1, degree2):
     print("The Res is: ", output)
     print("The Degree is: ", finalDegree)
 
-#polynomialAddition([3,1,9], [2,1,2,1], 2, 3)
+# polynomialAddition([3,1,9], [2,1,2,1], 2, 3)
 
 # b) Given the following pseudocode, first understand how it works, then implement the code for multiplying the two polynomials in the programming language of your choice
 
@@ -33,9 +33,9 @@ def polynomialMultiplication(p1, p2, degree1, degree2):
 
     for i in range(len(p1)):
         for j in range(len(p2)):
-            res[i+j] = res[i+j] + (p1[i] * p2[j])
-    
+            res[i + j] = res[i + j] + (p1[i] * p2[j])
+
     print(res)
 
 
-polynomialMultiplication([3, 1, 9], [2, 1, 2, 1], 2, 3)
+#polynomialMultiplication([3, 1, 9], [2, 1, 2, 1], 2, 3)
