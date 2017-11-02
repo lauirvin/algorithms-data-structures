82aa295e9ede3fa7f4230f2ee09f489221395419
lauirvin <lauirvin98@gmail.com>
2017-11-02 17:42:14 +0000

    Week4.py final commit

--- Week_4/Week4.py
+++ Week_4/Week4.py
@@ -45,9 +45,44 @@ def selectionSort(l):
         var = l[i]
         l[i] = l[maxIndex]
         l[maxIndex] = var
-   
 
 
 # l = [2, 7, 9, 4, 1, 5, 3, 6, 0, 8]
 # selectionSort(l)
 # print(l)
+
+def guessingGame():
+    import random as random
+    binary = False                    
+    lowNumber, highNumber = 1, 20000
+
+    number = random.randint(lowNumber, highNumber)
+    print("A number has been selected from", lowNumber, "and", highNumber)
+
+    lo = 1
+    hi = highNumber
+    guesses = 0
+
+    for i in range(lowNumber, highNumber):
+        #guess = int(input("What is your guess: "))
+        if binary:
+            guess = lo + (hi - lo) // 2     # integer division
+        else:
+            guess = random.randint(lo, hi)
+        print("Guess:", guess)
+        guesses += 1                      
+        
+        #check the guessed number
+        if guess > number:
+            print("Lower:")
+            hi = guess                        # bring down the upper bound
+        elif guess < number:
+            print("Higher:")
+            lo = guess                        # push up the lower bound
+        else:
+            break
+
+    print("That took", guesses, "guesses")
+    print("That took {0} guesses".format(guesses))
+
+guessingGame()
